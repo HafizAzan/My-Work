7733cc323213bc86709c607fb9bcb184d55cd242
unknown <HafizAzan/MY-WORK>
2024-02-18 14:14:12 +0500

    HTML-CSS-Responsice.CSS

---
+++ 1-HTMl-CSS/12-green-layout in Tailwind-CSS/tailwind.config-custom.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+      primarybackground: "#151515",
+      primary: "#008000",
+      secondary: "#43a508",
+      headingcolor: "#88888b",
+      }
+    },
+  },
+  plugins: [],
+}
+
