e47b15d5bf04124f53076d7d3d2c364620dcd8b4
Hafiz <azan09068@gmail.com>
2024-11-21 00:00:27 -0800

    next js task list

--- 7-Next JS/todo-list-app/app/utils/MainApiService.js
+++ 7-Next JS/todo-list-app/app/utils/MainApiService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
 
 const axiosIntance = axios.create({
   baseURL: BASE_URL,
